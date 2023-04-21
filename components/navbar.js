502d5d013d3b59e2124c64fc62f6d80a5e436b8e
Graeme Paterson <ghpaterson@gmail.com>
2023-04-21 11:08:18 +0100

    css changes to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import zombie from "../public/zombie.png";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <div className="flex justify-between py-4 mx-6">
-      <h1>HeadLess Blog</h1>
-      {router.pathname === "/" ? null : <Link href="/">Home</Link>}
+    <div className="flex justify-between items-center py-4 mx-10">
+      <div className="flex items-center">
+        <Image src={zombie} width={200} />
+        <h1 className="text-purple-600 text-6xl">HeadLess Blog</h1>
+      </div>
+      {router.pathname === "/" ? null : (
+        <div className="bg-purple-600 py-2 px-4 rounded-lg text-white text-4xl">
+          <Link href="/">Home</Link>
+        </div>
+      )}
     </div>
   );
 }
