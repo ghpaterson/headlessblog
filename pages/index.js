c3323cc84cb529080602e69fb54e8351d504169a
Graeme Paterson <ghpaterson@gmail.com>
2023-04-27 16:22:07 +0100

    smile added

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import BlogCard from "@/components/BlogCard";
 import headless from "../public/headless.svg";
 import herotext1 from "../public/herotext1.svg";
 import rectangle from "../public/rectangle.svg";
+import smile from "../public/smile.svg";
 
 const graphcms = new GraphQLClient(
   "https://api-eu-west-2.hygraph.com/v2/clgp7dx43075z01rrhdqcerg3/master"
@@ -48,15 +49,16 @@ export default function Home({ posts }) {
       <section className="w-full h-[500px] md:h-[600px] bg-[url('https://images.pexels.com/photos/1231258/pexels-photo-1231258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] bg-cover ">
         <div className="mx-60 py-20">
           <Image src={headless} width={1000} />
-          <div className=" flex py-48 -ml-16 gap-14">
+          <div className=" flex py-40 -ml-24 gap-10">
             <Image src={herotext1} width={800} />
-            <div>
-              <Image className="-mt-16" src={rectangle} height={300} />
+            <div className="flex gap-10">
+              <Image className="" src={rectangle} height={300} />
+              <Image className="pb-20" src={smile} width={200} />
             </div>
           </div>
         </div>
       </section>
-      <div className="flex justify-center py-6">
+      <div className="flex justify-center py-20">
         <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-12 mx-20 my-10">
           {posts.map((post) => (
             <BlogCard
